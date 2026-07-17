49d3581097158de1b1a90d3fe2087b21e9c71f0c
kubo-03 <chochocholfhin@gmail.com>
2026-07-17 21:22:48 +0900

    軽微な修正

--- main.py
+++ main.py
@@ -9,21 +9,12 @@ load_dotenv()
 
 async def main():
 
-    # ツールを使って計算する例
-    # calculate_tools_response = calculate_tools.run_agent(
-    #     "10 と 20 を掛け算してください。"
-    # )
-    # print(calculate_tools_response)
-
-    # ニュース記事を取得する例
-
-    # print(result)
     try:
         news_summary = await run_agent.run_agent("AI")
         print(news_summary)
 
     except Exception as e:
-        print("run_agent:", e)
+        print(e)
 
 
 if __name__ == "__main__":
